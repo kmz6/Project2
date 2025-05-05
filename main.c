6a069eef5c8330713e7a161ddb56bde5fc073aec
gpdnjs8 <jade3735@naver.com>
2025-05-05 14:35:28 +0900

    Implement datecmp

--- main.c
+++ main.c
@@ -99,6 +99,14 @@ void blockcmp()
 // 두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp()
 {
+    printf("date compare\n");
+    if(time1->tm_mon == time2->tm_mon){
+        if(time1->tm_mday < time2->tm_mday) printf("text1 is early\n\n");
+        else if(time1->tm_mday > time2->tm_mday) printf("text2 is early\n\n");
+        else printf("same date\n\n");
+    }
+    else if(time1->tm_mon < time2->tm_mon) printf("text1 is early\n\n");
+    else printf("text2 is early\n\n");
 }
 
 // 두 개의 파일 수정 시간을 비교하는 함수 작성
